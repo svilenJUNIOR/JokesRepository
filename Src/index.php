3358dd1f51a9487b022aabe82303d993c4f7b8c0
svilen iordanov <svilenborianov@gmail.com>
2022-07-26 12:05:11 +0200

    Connected to the database

--- Src/index.php
+++ Src/index.php
@@ -1,3 +1,16 @@
-<?php
-    echo "slabala";
-?>
+<?php include "../Config/DbContext.php"; ?>
+
+<form method="POST">
+<div>
+        <label for="Email">Email: </label>
+        <input type="text" name="Email"></input><br>
+
+        <label for="Password">Password: </label>
+        <input type="password" name="Password"></input><br>
+
+        <label for="Username">Username: </label>
+        <input type="text" name="Username"></input><br>
+
+        <button type="submit" name="Submit">Register</button>
+    </div>
+</form>
