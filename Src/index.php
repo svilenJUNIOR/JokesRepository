9a5fedbfdeb06618280ce53e41bd54e06fc2b768
svilen iordanov <svilenborianov@gmail.com>
2022-07-26 12:29:33 +0200

    Successfully added user in database

--- Src/index.php
+++ Src/index.php
@@ -1,6 +1,31 @@
 <?php
     include "../Config/DbContext.php";
     session_start();
+
+    if (isset($_POST["Submit"])) {
+        $Email = filter_input(INPUT_POST, "Email", FILTER_SANITIZE_SPECIAL_CHARS);
+        $Password = filter_input(INPUT_POST, "Password", FILTER_SANITIZE_SPECIAL_CHARS);
+        $Username = filter_input(INPUT_POST, "Username", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        echo $Email;
+        echo "<br>";
+        echo $Password;
+        echo "<br>";
+
+        echo $Username;
+
+        if (!empty($Email) && !empty($Password) && !empty($Username)) {
+            $sql = "INSERT INTO users (Email, Password, UserName) VALUES 
+            ('$Email', '$Password', '$Username')";
+
+            if (mysqli_query($connectionString, $sql)) header("Location: WelcomePage.php");
+            else Echo "Error ". mysqli_error($connectionString);
+        }
+    }
+    // get data from form
+    // create session
+    // create user
+    // add user
 ?>
 
 <form method="POST">
