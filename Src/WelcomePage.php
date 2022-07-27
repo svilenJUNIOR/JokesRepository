921fd2b2a2d14c24d3aa0a9c03dec734b6706592
svilen iordanov <svilenborianov@gmail.com>
2022-07-27 10:32:42 +0200

    Welcome page css

--- Src/WelcomePage.php
+++ Src/WelcomePage.php
@@ -1,26 +1,31 @@
-<?php
-    session_start();
-    include "../Config/DbContext.php";
+<style>
+    <?php include "./Style/WelcomePage.css"; ?>
+</style>
 
-    if (isset($_SESSION["UserId"])){
+<?php
+session_start();
+include "../Config/DbContext.php";
+
+
+if (isset($_SESSION["UserId"])) {
     echo "
     <h1> write something funny here so we can have a little bit of a laugh</h1>
     <form method='POST'>
     <textarea cols='50' rows='10' name='Joke'></textarea>
-    <button type='submit' name='Submit'>press</button>
-    </form>
-    ";
-    echo "<a href='logout.php'>log out</a>";
+    <div class='Wrapper'>
+    <button type='submit' name='Submit' class='SubmitButton'>Submit Joke</button>
+    <a href='logout.php' class='outButton'>log out</a>
+    </div>
+    </form>";
 
-
-    if (isset($_POST["Submit"])){
+    if (isset($_POST["Submit"])) {
         $Joke = filter_input(INPUT_POST, "Joke", FILTER_SANITIZE_SPECIAL_CHARS);
 
         if (!empty($Joke)) {
             $sql = "INSERT INTO jokes (text) VALUES ('$Joke')";
 
             if (mysqli_query($connectionString, $sql)) header("Location: WelcomePage.php");
-            else Echo "Error ". mysqli_error($connectionString);
+            else echo "Error " . mysqli_error($connectionString);
         }
     }
 
@@ -28,9 +33,11 @@
     $result = mysqli_query($connectionString, $query);
     $jokes = mysqli_fetch_all($result, MYSQLI_ASSOC);
     echo "<br>";
-    
-    for ($index = 0; $index < count($jokes);  $index++){
-        print_r($jokes[$index]["text"]);
+
+    for ($index = 0; $index < count($jokes); $index++) {
+        echo "<li>";
+        echo $jokes[$index]["text"];
+        echo "</li>";
         echo "<br>";
     }
 }
